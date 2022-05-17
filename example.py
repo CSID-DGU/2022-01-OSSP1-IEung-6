fa393c37e4bb0c388755e6dcc49d5cbd94228b03
lightorange0v0 <yeonjoo99@gmail.com>
2022-05-17 22:56:20 +0900

    Fix: 머지 전

--- example.py
+++ example.py
@@ -2,8 +2,7 @@
 Demonstration of the GazeTracking library.
 Check the README.md for complete documentation.
 """
-import sys
-#import json      2 send data to js (연주)
+import sys #연주
 import cv2
 from gaze_tracking import GazeTracking
 gaze = GazeTracking()
@@ -18,6 +17,7 @@ while True:
 
     frame = gaze.annotated_frame()
     text = ""
+
 
     if gaze.is_right():
         text = "Looking right"
@@ -34,18 +34,18 @@ while True:
         cv2.destroyAllWindows()
 
     cv2.putText(frame, text, (20, 60), cv2.FONT_HERSHEY_DUPLEX, 1.0, (147, 58, 31), 2)
-    cv2.putText(frame, "horizontal : "+str(gaze.horizontal_ratio()), (20, 90), cv2.FONT_HERSHEY_DUPLEX, 1.0, (147, 58, 31), 2)
+    #cv2.putText(frame, "horizontal : "+str(gaze.horizontal_ratio()), (20, 90), cv2.FONT_HERSHEY_DUPLEX, 1.0, (147, 58, 31), 2)
+    #cv2.putText(frame, "vertical : "+str(gaze.vertical_ratio()), (20, 120), cv2.FONT_HERSHEY_DUPLEX, 1.0, (147, 58, 31), 2)
     
-    left_pupil = gaze.pupil_left_coords()
-    right_pupil = gaze.pupil_right_coords()
-    cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 140), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
-    cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 185), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+    #left_pupil = gaze.pupil_left_coords()
+    #right_pupil = gaze.pupil_right_coords()
+    #cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 140), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+    #cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 185), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
 
     cv2.imshow("Demo", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-   
    
 webcam.release()
 cv2.destroyAllWindows()
